11b2f551a38e40a9c4518fad0748d50992670674
DotNetCraft <DotNetCraft@gmail.com>
2017-03-25 19:59:32 +1100

    Updated DotNetCraft.* NuGet package for the Example project

--- DotNetCraft.ConfigurationParser.Example/CustomStrategies/SplitRecipientsCustomStrategy.cs
+++ DotNetCraft.ConfigurationParser.Example/CustomStrategies/SplitRecipientsCustomStrategy.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Xml;
-using ConfigurationParser.Mapping;
+using DotNetCraft.ConfigurationParser.Mapping.Strategies;
 
 namespace DotNetCraft.ConfigurationParser.Example.CustomStrategies
 {
--- DotNetCraft.ConfigurationParser.Example/Settings/Smtp/SmtpSettings.cs
+++ DotNetCraft.ConfigurationParser.Example/Settings/Smtp/SmtpSettings.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using ConfigurationParser.Attributes;
+using DotNetCraft.ConfigurationParser.Attributes;
 using DotNetCraft.ConfigurationParser.Example.CustomStrategies;
 
 namespace DotNetCraft.ConfigurationParser.Example.Settings.Smtp
